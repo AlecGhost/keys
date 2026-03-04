8c9929e7c82ace06a97d3e4af7bb45a59f729212
shopfes <shopfes@outlook.de>
2026-03-04 19:21:22 +0100

    add header

--- src/client/java/de/ghost/KeysSettingScreen.java
+++ src/client/java/de/ghost/KeysSettingScreen.java
@@ -2,19 +2,30 @@ package de.ghost;
 
 import net.minecraft.client.gui.GuiGraphics;
 import net.minecraft.client.gui.components.Button;
+import net.minecraft.client.gui.components.EditBox;
 import net.minecraft.client.gui.components.toasts.SystemToast;
 import net.minecraft.client.gui.screens.Screen;
 import net.minecraft.network.chat.Component;
 
 public class KeysSettingScreen extends Screen {
+	int headerY = 40;
 	public KeysSettingScreen(Component title) {
 		super(title);
 	}
 
 	@Override
+	public void render(GuiGraphics graphics, int mouseX, int mouseY, float delta) {
+		Component header = Component.literal("Key Bind Profiles");
+		super.render(graphics, mouseX, mouseY, delta);
+		graphics.drawString(this.font, header, this.width / 2 - this.font.width(header) / 2, this.headerY - this.font.lineHeight - 10, 0xFFFFFFFF, true);
+	}
+
+	@Override
 	protected void init() {
+		EditBox editBox = new EditBox(this.font, 40, 40 + 30, 200, 20, Component.literal("Type here"));
+		this.addRenderableWidget(editBox);
+
 		Button buttonWidget = Button.builder(Component.literal("Hello World"), (btn) -> {
-			// When the button is clicked, we can display a toast to the screen.
 			this.minecraft.getToastManager().addToast(
 					SystemToast.multiline(this.minecraft, SystemToast.SystemToastId.NARRATOR_TOGGLE,
 							Component.nullToEmpty("Hello World!"), Component.nullToEmpty("This is a toast.")));
@@ -23,9 +34,4 @@ public class KeysSettingScreen extends Screen {
 
 	}
 
-	@Override
-	public void render(GuiGraphics graphics, int mouseX, int mouseY, float delta) {
-		super.render(graphics, mouseX, mouseY, delta);
-		graphics.drawString(this.font, "Special Button", 40, 40 - this.font.lineHeight - 10, 0xFFFFFFFF, true);
-	}
 }
